e0284e54326c3b58935dd3cf599d0dcfe4f19769
absonkab <kyab99@gmail.com>
2022-02-03 16:20:54 +0000

    Compiles assets for Github Pages

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -2,4 +2,5 @@ rsync -r src/ docs/
 rsync  build/contracts/* docs/
 git add .
 git commit -m "Compiles assets for Github Pages"
+git remote add origin git@github.com:absonkab/ABT_token_sale.git
 git push -u origin master
